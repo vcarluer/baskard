1e1d15dbe04e8e305a04477536fa4672c943d69f
Vincent Carluer <vcarluer@gmail.com>
2016-01-15 21:21:56 +0000

    Reinitialize

---
+++ httpServer.js
@@ -0,0 +1,15 @@
+var http = require("http");
+http.createServer(function(request, response) {
+    response.writeHead("200", { "content-type": "text/html"});
+    response.write("<!DOCTYPE html>");
+    response.write("<html>");
+    response.write("<head>");
+    response.write("<title>Hello World Page</title>");
+    response.write("</head>");
+    response.write("<body>");
+    response.write("Hello World!");
+    response.write("</body>");
+    response.write("</html>");
+    response.end();
+}).listen(process.env.PORT, process.env.IP);
+console.log("Server is listening on " + process.env.IP + ":" + process.env.PORT);
