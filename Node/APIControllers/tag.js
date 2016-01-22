640aa0ed9ddbcd258234ebb771e44662465ad6b4
Vincent Carluer <vcarluer@gmail.com>
2016-01-22 13:05:15 +0000

    dummy commit

--- Node/APIControllers/tag.js
+++ Node/APIControllers/tag.js
@@ -3,6 +3,10 @@ var pg = require('pg');
 var connectionString = "postgres://fvtjauwobkigbf:8reHZ_30Uj-6js4s1nY66ktN0l@ec2-54-247-170-228.eu-west-1.compute.amazonaws.com:5432/d6t3vp05h61n8r?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
 var tag = function() {};
 tag.get = function(response, body, request, id) {
+    if (id) {
+        
+    }
+    
     pg.connect(connectionString, function(err, client, done) {
        if(err) {
             return console.error('error fetching client from pool', err);
